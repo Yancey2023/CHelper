105f39d11dc8e1eaf5f07ce39acb860960c611be
Yancey <1709185482@qq.com>
2026-03-14 13:37:55 +0800

    bump version v0.4.1

--- CHelper-Android/app/build.gradle.kts
+++ CHelper-Android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "yancey.chelper"
         minSdk = 24
         targetSdk = 36
-        versionCode = 74
-        versionName = "0.4.0"
+        versionCode = 75
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
